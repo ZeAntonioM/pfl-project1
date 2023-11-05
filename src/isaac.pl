2849e0d3db6e9044b673b39692d8f3962d3644e7
ZeAntonioM <joseapmartins2003@gmail.com>
2023-11-05 02:52:55 +0000

    Finished documentation on validators, utils, isaac and io

--- src/isaac.pl
+++ src/isaac.pl
@@ -1,8 +1,10 @@
 :-ensure_loaded('game_states.pl').
 
+% Play calls the main menu
 play:- isaac_menu.
 
-
+% Main menu
+% draws the menu, reads the input and calls the gamemode. If the input is 10, the game ends. 
 isaac_menu :- repeat,
              draw_isaac_menu,
              ask_for_menu_option(Selection),
@@ -12,20 +14,37 @@ isaac_menu :- repeat,
                  gamemode(Selection),fail
              ).
 
- %player_robot(Player, easy)
+%player_robot(Player, easy)
 %player_robot(Player,hard)
 
+% 1 - Player vs Player
 gamemode(1) :- play_game.
 
+% 2 - Player vs Easy IA
 gamemode(2) :- play_game.
+
+% 3 - Easy IA vs Player
 gamemode(3) :- play_game.
+
+% 4 - Player vs Hard IA
 gamemode(4) :- play_game.
+
+% 5 - Hard IA vs Player
 gamemode(5) :- play_game.
+
+% 6 - Easy IA vs Hard IA
 gamemode(6) :- play_game.
+
+% 7 - Hard IA vs Easy IA
 gamemode(7) :- play_game.
+
+% 8 - Easy IA vs Easy IA
 gamemode(8) :- play_game.
+
+% 9 - Hard IA vs Hard IA
 gamemode(9) :- play_game.
 
+% play_game prepares the game and starts it
 play_game :-
             retractall(piece_position(_,_,_)),
             retractall(sc(_,_)),
@@ -39,11 +58,13 @@ play_game :-
             %populate,
             game_state(start, "W").
     
-
+% game_state(+GameState, +Player) is the main game loop
+% if the game is over, it prints the winner
 game_state(GameState, _):-
        game_over(GameState, Winner),!,
        congrats(Winner).
 
+% if the game is not over, it displays the board, asks for a move, makes the move and changes the player
 game_state(GameState, Player):-
     display(GameState, Player),
     ask_for_move(GameState,Player,Move),
@@ -51,31 +72,37 @@ game_state(GameState, Player):-
     change_player(NewGameState,Player, Next_Player),!,
     game_state(NewGameState, Next_Player).
 
-
+% debug questions
 game_state(_,_):-write('Fail').
 
+% game_over(+GameState, -Winner) checks if the game is over and returns the winner
+% If the game is on the 2nd phase and the Score Counter of the White Player is 100, then the winner is the White Player
 game_over(GameState,"White"):-
     (GameState= both_players_remove;
      GameState = one_player_remove
     ),
     sc("W",100).
 
+% If the game is on the 2nd phase and the Score Counter of the Black Player is 100, then the winner is the Black Player
 game_over(GameState,"Black"):-
     (GameState= both_players_remove;
      GameState = one_player_remove
     ),
     sc("B",100).
 
+% If the second phase ended, if the White Player has more points than the Black Player, then the winner is the White Player
 game_over(end_game,"White"):-
     sc("B",SCB),
     sc("W",SCW),
     SCW > SCB.
 
+% If the second phase ended, if the Black Player has more points than the White Player, then the winner is the Black Player
 game_over(end_game,"Black"):-
     sc("B",SCB),
     sc("W",SCW),
     SCW < SCB.
 
+% congrats(+Winner) prints the winner
 congrats(Winner):-
     nl,
     print_string_(Winner),
--- src/validators.pl
+++ src/validators.pl
@@ -1,19 +1,22 @@
 :-ensure_loaded('utils.pl').
 :- use_module(library(between)).
 
-
+% validate_size(+Size) checks if the size is between 3 and 7
 validate_size(Size):-
         between(3,7,Size).
 
+% validate_direction(+Direction) checks if the direction is one of the four possible directions
 validate_direction(Direction):-
         Direction = d;
         Direction = u;
         Direction = l;
         Direction = r.
 
+% validate_position(+Position) checks if the position is between 0 and 99
 validate_position(Position):-
         between(0,99,Position).
 
+% valid_piece(+Player, +Size, -Piece) returns a valid piece for the player and size if exists
 valid_piece( Player, Size, Piece) :-
     first_piece_of_size(Size, Player, First_Piece),
     Size2 is Size + 1,
@@ -21,11 +24,14 @@ valid_piece( Player, Size, Piece) :-
     !,
     next_piece_not_on_board( First_Piece, Last_Piece, Piece).
 
-
+% valid_position(+Size, +Position, +Direction) checks if the position is valid for the size and direction
+% If the size is 1, it checks if the position is valid and if there is no piece in that position
 valid_position(1,Position,_):-
         validate_position(Position),
         \+ piece_position(_Piece,_Direction,Position).
         
+% If the size is bigger than 1 and the direction is left or right, it checks if the position is valid and if there is no piece in that position, and
+% if the next position is inside the board and in the same line.
 valid_position(Size,Position,Direction):-
         (Direction = l;
         Direction = r),
@@ -38,6 +44,8 @@ valid_position(Size,Position,Direction):-
         Next_line = Line,
         valid_position(Size2,Next_position,Direction).
 
+% If the size is bigger than 1 and the direction is up or down, it checks if the position is valid and if there is no piece in that position, and
+% if the next position is inside the board and in the same column.
 valid_position(Size,Position,Direction):-
          (Direction = u;
         Direction = d),
@@ -47,14 +55,17 @@ valid_position(Size,Position,Direction):-
         Size2 is Size -1,
         valid_position(Size2,Next_position,Direction).
 
+% validate_menu_choice(+Choice) checks if the choice is between 1 and 10
 validate_menu_choice(Choice):-
         Choice >= 1,
         Choice =< 5.
 
+% validate_piece_to_remove(+Piece) checks if the piece is on the board
 validate_piece_to_remove(Piece):-
         piece_position(Piece,_Direction,_Position).
 
-
+% can_move_piece(+Player, +Piece, +Direction) checks if the player is the owner of the piece, gets the piece positions, the Score Counters positions
+% and the Biggest Piece removed, and checks if the piece size is bigger than the Biggest Piece removed and has no Score Counters in the top of the piece
 can_remove_piece(Player,Piece):-
     piece_owner(Piece,Player),
     findall(Position, piece_position(Piece, _, Position),  Positions),
